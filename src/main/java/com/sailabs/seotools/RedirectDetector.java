95b005af4cf8f5841973ce143d083aaa4a15529a
Sai Hegde <saihegde@gmail.com>
2013-05-19 14:29:56 -0600

    Updated - Some code refactoring

--- src/main/java/com/sailabs/seotools/RedirectDetector.java
+++ src/main/java/com/sailabs/seotools/RedirectDetector.java
@@ -1,24 +1,21 @@
 package com.sailabs.seotools;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.client.params.HttpClientParams;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -26,104 +23,55 @@ public class RedirectDetector {
 
 	public String getRedirectedURL(String url) {
 		try {
-			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-			System.out.println("Request URL ... " + url);
-			conn.connect();
-			conn.getInputStream();
-
-			// TODO
-			String newUrl = conn.getURL().toURI().toString();
-
-			System.out.println("Redirect to URL : " + newUrl);
-			return newUrl;
-
-		} catch (MalformedURLException e) {
+			HttpClient client = new DefaultHttpClient();
+			HttpParams params = client.getParams();
+			HttpClientParams.setRedirecting(params, false);
+			HttpGet method = new HttpGet(url);
+			HttpResponse resp;
+			resp = client.execute(method);
+			String location = resp.getLastHeader("Location") == null ? null : resp.getLastHeader("Location").getValue();
+			return location;
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	public static void readXLSFile(String filePath) throws IOException {
+	public List<Row> readXLSFile(String filePath) throws IOException {
 		InputStream ExcelFileToRead = new FileInputStream(filePath);
 		XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
 		XSSFSheet sheet = wb.getSheetAt(0);
-		XSSFRow row;
-		XSSFCell cell;
-		Iterator<Row> rows = sheet.rowIterator();
-		while (rows.hasNext()) {
-			row = (XSSFRow) rows.next();
-			RedirectDetector rd = new RedirectDetector();
-			rd.getRedirectedURL(row.getCell(0).getStringCellValue());
-		}
+		// XSSFRow row;
+		// XSSFCell cell;
+		Iterator<Row> iterator = sheet.rowIterator();
+		List<Row> rows = new ArrayList<Row>();
+		while (iterator.hasNext())
+			rows.add((Row) iterator.next());
+
+		return rows;
 	}
 
-	public static void main(String[] args) throws IOException, URISyntaxException {
-		// readXLSFile("C:/Users/shegde/Downloads/301redirects.xlsx");
+	public static void main(String[] args) {
 		RedirectDetector rd = new RedirectDetector();
-		URL url = new URL("http://www.directvaloans.com/Default.aspx");
-		HttpClient httpclient = new DefaultHttpClient();
 		try {
-			HttpGet httpget = new HttpGet("http://www.directvaloans.com/Default.aspx");
-			System.out.println("executing request " + httpget.getURI());
-			// Create a response handler
-			ResponseHandler<String> responseHandler = new BasicResponseHandler();
-			String responseBody = httpclient.execute(httpget, responseHandler);
-			System.out.println("----------------------------------------");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String inputLine;
-			while ((inputLine = in.readLine()) != null)
-				System.out.println(inputLine);
-			in.close();
-			System.out.println("----------------------------------------");
-		} finally {
-			// When HttpClient instance is no longer needed,
-			// shut down the connection manager to ensure immediate deallocation
-			// of all system resources
-			httpclient.getConnectionManager().shutdown();
+			for (Row row : rd.readXLSFile("C:/Users/Sai/Downloads/301redirects.xlsx")) {
+				System.out.println("Original URL: " + row.getCell(0).getStringCellValue());
+				System.out.println("Redirected URL: " + rd.getRedirectedURL(row.getCell(0).getStringCellValue()));
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-
-	// public static void main(String[] args) {
-	// // Create and enter a Context. A Context stores information about the
-	// // execution environment of a script.
-	// Context cx = Context.enter();
-	//
-	// try {
-	// // Initialize the standard objects (Object, Function, etc.). This
-	// // must be done before scripts can be
-	// // executed. The null parameter tells initStandardObjects
-	// // to create and return a scope object that we use
-	// // in later calls.
-	// Scriptable scope = cx.initStandardObjects();
-	//
-	// // Build the script
-	// String script =
-	// "window.open('http://www.directvaloans.com/Default.aspx')";
-	//
-	// // Execute the script
-	// Object obj = cx.evaluateString(scope, script, "TestScript", 1, null);
-	// System.out.println("Object: " + obj);
-	//
-	// // Cast the result to a string
-	// System.out.println((String) obj);
-	//
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// } finally {
-	// // Exit the Context. This removes the association between the
-	// // Context and the current thread and is an
-	// // essential cleanup action. There should be a call to exit for
-	// // every call to enter.
-	// Context.exit();
-	// }
-	// }
 
 }
