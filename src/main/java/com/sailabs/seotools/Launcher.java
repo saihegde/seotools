ed6cfb9ce1fb78954e1970409c9b02fa18b9184e
Sai Hegde <saihegde@gmail.com>
2013-05-24 09:44:39 -0600

    Refactored launchers

--- src/main/java/com/sailabs/seotools/Launcher.java
+++ src/main/java/com/sailabs/seotools/Launcher.java
@@ -1,20 +1,48 @@
 package com.sailabs.seotools;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.poi.ss.usermodel.Row;
+
+import au.com.bytecode.opencsv.CSVWriter;
 
 public class Launcher {
 
 	public static void main(String[] args) throws IOException {
-		Document document = Jsoup.connect("https://www.directvaloans.com").get();
-		String title = MetatagReader.getMetaTag(document, "title");
-		String description = MetatagReader.getMetaTag(document, "description");
-		System.out.println("title: " + title);
-		System.out.println("description: " + description);
-		// and others you need to
-		//String ogImage = getMetaTag(document, "og:image") 
+		String inputFilePath = null;
+		String csvFilePath = null;
+		if (args.length >= 1){
+			inputFilePath = args[0] != null ? args[0] : "C:/Users/shegde/Downloads/301redirects.xlsx";
+		}
+		if (args.length >= 2){
+			csvFilePath = args[1] != null ? args[1] : "C:/temp/rdoutput.csv";
+		}
+		CSVWriter csvWriter = new CSVWriter(new FileWriter(csvFilePath));
+		
+		List<String[]> data = new ArrayList<String[]>();
+		RedirectDetector rd = new RedirectDetector();
+		SpreadsheetReader spreadsheetReader = new SpreadsheetReader();
+		System.out.println("Processing input : " + inputFilePath);
+		try{
+			try {
+				for (Row row : spreadsheetReader.read(inputFilePath)) {
+					String url = row.getCell(0).getStringCellValue();
+					data.add(new String[]{url, rd.getRedirectedURL(url)});
+				}
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			csvWriter.writeAll(data);
+		} finally {
+			csvWriter.close();
+			System.out.println("The file has been processed and the output written to : " + csvFilePath);
+		}
 	}
 	
 }
--- src/main/java/com/sailabs/seotools/RedirectDetector.java
+++ src/main/java/com/sailabs/seotools/RedirectDetector.java
@@ -5,29 +5,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.GeneralSecurityException;
 
-import org.apache.http.client.ClientProtocolException;
-import org.apache.poi.ss.usermodel.Row;
-
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class RedirectDetector {
-
-	public static void main(String[] args) {
-		RedirectDetector rd = new RedirectDetector();
-		SpreadsheetReader spreadsheetReader = new SpreadsheetReader();
-		try {
-			for (Row row : spreadsheetReader.read("C:/Users/shegde/Downloads/301redirects.xlsx")) {
-				System.out.println("Original URL: " + row.getCell(0).getStringCellValue());
-				System.out.println("Redirected URL: " + rd.getRedirectedURL(row.getCell(0).getStringCellValue()));
-			}
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
 	@SuppressWarnings("deprecation")
 	public String getRedirectedURL(String url) {
@@ -48,7 +30,7 @@ public class RedirectDetector {
 		} catch (FailingHttpStatusCodeException e) {
 			e.printStackTrace();
 		} catch (MalformedURLException e) {
-			e.printStackTrace();
+			return "Not a valid URL";
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
